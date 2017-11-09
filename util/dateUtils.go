a8e3c49564f9a51dfbf0d2669c245b438ece2bee
yujianjian <yujianjian@new4g.cn>
2017-11-09 19:28:33 +0800

    add calculate actual amount method

--- util/dateUtils.go
+++ util/dateUtils.go
@@ -1,7 +1,21 @@
 package util
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
-func GetTimeAddEight(date time.Time) (time.Time) {
-	return date.Add(8*time.Hour)
+func GetTimeAddEight(date time.Time) time.Time {
+	return date.Add(8 * time.Hour)
+}
+
+// calculate actual amount
+func GetActualAmount(amount *int64) float64 {
+	amountString := "0"
+	if amount != nil {
+		amountString = strconv.FormatInt(*amount, 64)
+	}
+	amountFloat, _ := strconv.ParseFloat(amountString, 64)
+	actualAmount := amountFloat / float64(100000)
+	return actualAmount
 }
