20d54b7da251dd4834903eac7e0e3864031bc8cd
yujianjian <yujianjian@new4g.cn>
2017-11-10 10:30:47 +0800

    deal with amount when the balance is zero

--- contracts/service/actBroswerService.go
+++ contracts/service/actBroswerService.go
@@ -1,11 +1,12 @@
 package service
 
 import (
-	"Browser-achain/contracts/models"
-	"github.com/gin-gonic/gin"
-	"Browser-achain/util"
-	"log"
 	"Browser-achain/common"
+	"Browser-achain/contracts/models"
+	"Browser-achain/util"
+	"github.com/gin-gonic/gin"
+	"log"
+	"strconv"
 )
 
 type UserBalanceVo struct {
@@ -13,7 +14,7 @@ type UserBalanceVo struct {
 	Balance  string
 }
 
-const coinType  = "ACT"
+const coinType = "ACT"
 
 // Check the balance of all currencies in the address according to the address
 func QueryBalanceByAddress(c *gin.Context) {
@@ -34,10 +35,18 @@ func QueryBalanceByAddress(c *gin.Context) {
 	for _, value := range list {
 		var userBalanceVo UserBalanceVo
 		userBalanceVo.CoinType = value.CoinType
-		userBalanceVo.Balance = util.GetActualAmount(value.Balance)
-		if coinType != userBalanceVo.CoinType && userBalanceVo.Balance == "0" {
+		actualAmountString := util.GetActualAmount(value.Balance)
+		actualAmount, _ := strconv.ParseFloat(actualAmountString, 32)
+
+		// if coinType is not ACT and balance less than 0,ignore
+		if coinType != userBalanceVo.CoinType && actualAmount <= float64(0) {
 			return
 		}
+		// if coinType is ACT and balance less than 0, replace with 0
+		if coinType == userBalanceVo.CoinType && actualAmount <= float64(0) {
+			actualAmount = float64(0)
+		}
+		userBalanceVo.Balance = strconv.FormatFloat(actualAmount, 'E', -1, 32)
 		userBalanceVoList = append(userBalanceVoList, userBalanceVo)
 	}
 	common.WebResultSuccess(userBalanceVoList, c)
